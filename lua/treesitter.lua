19c234e7265d73be3b8e382bc683f87e2b40376d
Ryan Hawkins <ryan.hawkins@instructure.com>
2023-01-05 11:31:56 -0700

    Add additional treesitter languages
    Added additional treesitter languages so it wouldn't complain so much
    when editing Vim files.

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,10 +1,10 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = {'javascript', 'typescript', 'ruby', 'go'},
+  ensure_installed = {'javascript', 'typescript', 'ruby', 'go', 'vim', 'lua'},
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
     disable = function(lang, buf)
-        local max_filesize = 250 * 1024 -- 100 KB
+        local max_filesize = 250 * 1024 -- 250 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
             return true
