1cf63b7e4ddb06cb4147d12708441cee28da126e
Ryan Hawkins <ryanlarryhawkins@gmail.com>
2023-03-30 12:36:58 -0600

    Even more formatting, even more plugins!
    Added plugins for DAP, as well as modified some of the config for
    null-ls so that certain formatters/checkers are installed by default.
    Also, turns out fzf-lua has a billion built-in pickers, including ones
    for nvim-dap! I've basically got a full running flutter dev setup now!

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,60 +12,62 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", opts)
 
 wk.register({
-  f = {
-    name = "Find Things",
-    f = { fzf.files, "Find Files" },
-    q = { fzf.quickfix, "Quickfix List" },
-    g = { fzf.live_grep_native, "Live Grep" },
-    s = { fzf.lsp_document_symbols, "Find Document Symbols" },
-    S = { fzf.lsp_workspace_symbols, "Find Workspace Symbols" },
-    b = { fzf.buffers, "Pick Buffer" },
-    h = { fzf.help_tags, "Find Help" },
-  },
-  t = {
-    name = "Trouble",
-    t = { "<cmd>TroubleToggle<CR>", "Toggle Trouble" },
-    q = { "<cmd>TroubleToggle quickfix<cr>", "Toggle Quickfix" },
-    d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Open Document Diagnostics" },
-  },
-  k = { vim.lsp.buf.signature_help, "Signature Help" },
-  r = { vim.lsp.buf.rename, "Rename Variable" },
-  e = { "<cmd>Neotree toggle<cr>", "Toggle Neotree" },
-  d = {
-    name = "Debugger",
-    b = { dap.toggle_breakpoint, "Toggle Breakpoint" },
-    c = { dap.continue, "Continue Execution" },
-    i = { dap.step_into, "Step Into" },
-    o = { dap.step_out, "Step Out" },
-    p = { dap_ui.preview, "Preview" },
-    f = {
-      function()
-        dap_ui.centered_float(dap_ui.frames)
-      end,
-      "View Frames",
-    },
-    h = { dap_ui.hover, "Hover" },
-    s = {
-      function()
-        dap_ui.centered_float(dap_ui.scopes)
-      end,
-      "Scopes",
-    },
-    r = { dap.repl.toggle, "Toggle REPL" },
-  },
+	f = {
+		name = "Find Things",
+		f = { fzf.files, "Find Files" },
+		q = { fzf.quickfix, "Quickfix List" },
+		g = { fzf.live_grep_native, "Live Grep" },
+		s = { fzf.lsp_document_symbols, "Find Document Symbols" },
+		S = { fzf.lsp_workspace_symbols, "Find Workspace Symbols" },
+		b = { fzf.buffers, "Pick Buffer" },
+		h = { fzf.help_tags, "Find Help" },
+	},
+	t = {
+		name = "Trouble",
+		t = { "<cmd>TroubleToggle<CR>", "Toggle Trouble" },
+		q = { "<cmd>TroubleToggle quickfix<cr>", "Toggle Quickfix" },
+		d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Open Document Diagnostics" },
+	},
+	k = { vim.lsp.buf.signature_help, "Signature Help" },
+	r = { vim.lsp.buf.rename, "Rename Variable" },
+	a = { vim.lsp.buf.code_action, "Code Action" },
+	e = { "<cmd>Neotree toggle<cr>", "Toggle Neotree" },
+	d = {
+		name = "Debugger",
+		b = { dap.toggle_breakpoint, "Toggle Breakpoint" },
+		l = { require("fzf-lua.providers.dap").breakpoints, "View All Breakpoints" },
+		c = { dap.continue, "Continue Execution" },
+		i = { dap.step_into, "Step Into" },
+		o = { dap.step_out, "Step Out" },
+		p = { dap_ui.preview, "Preview" },
+		f = {
+			function()
+				dap_ui.centered_float(dap_ui.frames)
+			end,
+			"View Frames",
+		},
+		h = { dap_ui.hover, "Hover" },
+		s = {
+			function()
+				dap_ui.centered_float(dap_ui.scopes)
+			end,
+			"Scopes",
+		},
+		r = { dap.repl.toggle, "Toggle REPL" },
+	},
 }, { prefix = "<leader>" })
 
 wk.register({
-  K = { vim.lsp.buf.hover, "Hover" },
-  g = {
-    r = { fzf.lsp_references, "Search References" },
-    d = { fzf.lsp_definitions, "Go-To Definition(s)" },
-    i = { fzf.lsp_implementations, "Go-To Implementation" },
-  },
-  c = {
-    p = {
-      r = { [[<cmd>let @+ = expand("%")<cr>]], "Copy Relative Path" },
-      a = { [[<cmd>let @+ = epand("%:p")<cr>]], "Copy Absolute Path" },
-    },
-  },
+	K = { vim.lsp.buf.hover, "Hover" },
+	g = {
+		r = { fzf.lsp_references, "Search References" },
+		d = { fzf.lsp_definitions, "Go-To Definition(s)" },
+		i = { fzf.lsp_implementations, "Go-To Implementation" },
+	},
+	c = {
+		p = {
+			r = { [[<cmd>let @+ = expand("%")<cr>]], "Copy Relative Path" },
+			a = { [[<cmd>let @+ = epand("%:p")<cr>]], "Copy Absolute Path" },
+		},
+	},
 })
